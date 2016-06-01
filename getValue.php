d195b71f5f41f532c4fd0837da738905837b065f
Jin Wook Kim <jinwook31@naver.com>
2016-06-01 09:31:32 +0900

    php file

---
+++ getValue.php
@@ -0,0 +1,11 @@
+<?php
+	$link = mysql_connect("localhost", "ID", "PW") or die ("Fail to connect MySQL");
+	$select_db = mysql_select_db("dbname", $link) or die ("Fail to connect DB");
+	
+	$val = $_GET["val"]; $ID = $_GET["ID"];
+	
+	$sqlt "insert into $ID (val, time) values ('$val','Now())')";
+	$db_result = mysql_query($sqlt, $link) or die ("Fail to connect Table");
+	
+	echo "Success!";
+?>
